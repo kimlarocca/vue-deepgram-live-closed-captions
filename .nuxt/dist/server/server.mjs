38f7e5dd84f2ec798860c3f0a571ddfdbce10f62
Wilfredo Colon <wil@equaltime.io>
2024-09-27 22:19:14 -0400

    Removing .nuxt dir from repo

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/kimlarocca/Websites/vue-deepgram-live-closed-captions/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- .nuxt/types/supabase.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '#supabase/server' {
-  const serverSupabaseClient: typeof import('/Users/kimlarocca/Websites/vue-deepgram-live-closed-captions/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
-  const serverSupabaseServiceRole: typeof import('/Users/kimlarocca/Websites/vue-deepgram-live-closed-captions/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
-  const serverSupabaseUser: typeof import('/Users/kimlarocca/Websites/vue-deepgram-live-closed-captions/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
-}
